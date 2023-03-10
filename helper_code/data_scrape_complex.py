e87c0fe1eb182aa186641f31d30e2faf6777b3af
Kai Kiwitz <kai.kiwitz@gmail.com>
2023-03-10 17:34:13 +0100

    added MARSAtlas/person/digitalidentifiers instances as well as helper code

---
+++ helper_code/data_scrape_complex.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL of the website containing the table
+url = 'https://meca-brain.org/software/marsatlas/'
+
+# Make a request to the website and get the HTML content
+response = requests.get(url)
+html_content = response.content
+
+# Use BeautifulSoup to parse the HTML content
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Find all table elements on the page
+tables = soup.find_all('table')
+
+# Create a dictionary to store the lists for each table
+tables_data = {}
+
+# Loop through each table and extract the data from its cells
+for index, table in enumerate(tables, 1):
+    # Create a list for each column of the table
+    num_columns = len(table.find_all('tr')[0].find_all('td'))
+    column_lists = [[] for _ in range(num_columns)]
+
+    # Loop through each row and extract the data from its cells
+    rows = table.find_all('tr')
+    for row in rows:
+        cells = row.find_all('td')
+
+        # Loop through each cell and append its contents to the corresponding column list
+        for i in range(num_columns):
+            column_lists[i].append(cells[i].text.strip())
+
+    # Store the lists for each table in the dictionary
+    tables_data[f"table_{index}"] = column_lists
+
+# Print the contents of each table
+for table_name, columns in tables_data.items():
+    print(f"Table Name: {table_name}")
+    for i, column in enumerate(columns):
+        print(f"Column {i+1}: {column}")
+
---
+++ helper_code/json_generation.py
@@ -0,0 +1,23 @@
+# customized scraping to get the right info
+print(tables_data["table_1"][2])
+region_names = tables_data["table_1"][2]
+print(region_names)
+region_names.remove(region_names[0])
+print(region_names)
+
+# generate json code for integration
+with open("/openMINDS_SANDS/helper_code/MarsAtlasRegions.txt", 'w') as f:
+    f.write("Mars_Areas\n")
+    open = "{"
+    close = "}"
+    instance = "\"@id\": \"https://openminds.ebrains.eu/instances/parcellationEntity/"
+
+    for index, area in enumerate(region_names):
+        f.write(f"{open}\n")
+        f.write(f"{instance}{region_names[index]}\"\n")
+        f.write(f"{close},\n")
+f.close()
+
+
+
+
