cca8f91511d92c8fc67febc34eb0b084e47a49c0
Kai Kiwitz <kai.kiwitz@gmail.com>
2023-03-23 14:14:14 +0100

    added istances for parcellation entity, works

--- helper_code/BrainAtlasVersionInstance.py
+++ helper_code/BrainAtlasVersionInstance.py
@@ -1,3 +1,25 @@
+# ______________________________________________________________________#
+# brain atlas versions created by hand first
+
+
+# ______________________________________________________________________#
+# brain tlas versions info created automatically (needs to be integrated into the by hand created#
+
+# data
+
+atlas_version_https = "https://openminds.ebrains.eu/instances/brainAtlasVersion/"
+accessibility_https = "https://openminds.ebrains.eu/instances/productAccessibility/freeAccess"
+atlas_type_https = "https://openminds.ebrains.eu/instances/atlasType/deterministicAtlas"
+coordinate_space_https = "https://openminds.ebrains.eu/instances/commonCoordinateSpace/"
+
+coordinate_spaces = ["Collin27_1998", "HipHop138"]
+
+abbreviation = "Mars"
+
+
+
+#brain atlas versions
+
 # schema creation brain atlas version
 marsAtlas = atlas.add_SANDS_brainAtlasVersion(coordinateSpace= {"@id": "https://openminds.ebrains.eu/instances/commonCoordinateSpaceVersion/MNI-ICBM152_nonlinear-2009c-asym"})
 
