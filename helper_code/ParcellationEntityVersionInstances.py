d04777ca10d8e2fcd78fb14dba9afb1cd34407e1
Kai Kiwitz <kai.kiwitz@gmail.com>
2023-03-30 10:30:50 +0200

    basic instances, atlas instance, atlas version instances, and parcellation entity instances completed,

--- helper_code/ParcellationEntityVersionInstances.py
+++ helper_code/ParcellationEntityVersionInstances.py
@@ -1,38 +1,27 @@
 import os
-# list of subcortical vs cortical from the scrype script
-# combine scrapes into unified datastructure
-entities = region_names_cortex + region_names_subcortex
-entities_label = [0] * (len(region_names_cortex)+len(region_names_subcortex))
-for index in range(len(entities_label)):
-    if index < len(region_names_cortex):
-        entities_label[index] = "c"
-    else:
-        entities_label[index] = "s"
-all_entitites = zip(entities, entities_label)
-all_entitites = list(all_entitites)
+import openMINDS.version_manager
+import json
+import glob
 
-# create percellation entity version directories
-atlas_versions = ["Mars_individual_cortex", "Mars_individual_cortexAndSubcortex", "Mars_HipHop138_cortex", "Mars_Colin27_1998_cortexAndSubcortex"]
-cortex_versions = ["Mars_individual_cortex", "Mars_HipHop138_cortex"]
-cortexAndSubcortex_versions = ["Mars_individual_cortexAndSubcortex", "Mars_Colin27_1998_cortexAndSubcortex"]
-path = "/home/kiwitz1/PycharmProjects/OpenMinds/openMINDS_SANDS/instances/atlas/parcellationEntityVersion/"
-j = ".jsonld"
-sep = "/"
-us = "_"
-print(all_entitites)
-for version in atlas_versions:
-    os.mkdir(f"{path}{version}")
 
-# create instance files for each parcellation entity version directory
-for area in all_entitites:
-    # create jsonlds for the subcortex atlases (they always include corttex and subcortex)
-    for version in cortexAndSubcortex_versions:
-        with open(f"{path}{version}{sep}{version}{us}{area[0]}{j}", "w") as z:
-            z.write("")
-    # create the atlases that only onvolve corticla parcellations
-    if area[1] == "c":
-        for version in cortex_versions:
-            with open(f"{path}{version}{sep}{version}{us}{area[0]}{j}", "w") as p:
-                p.write("")
 
-# to
+
+
+
+
+if __name__ == '__main__':
+
+    # directories and variables
+    entity_dir = f"/home/kiwitz1/PycharmProjects/OpenMinds/openMINDS_SANDS/instances/atlas/parcellationEntity/{abbreviation}/"
+    os.mkdir(entity_dir)
+    j = ".jsonld"
+    p = "./instances/"
+
+    # intialize openMinds instance creator
+    openMINDS.version_manager.init()
+    openMINDS.version_manager.version_selection('v3')
+    helper = openMINDS.Helper()
+    basic = helper.create_collection()
+
+    # function calling
+    generate_entities(entity_dir, versions, abbreviation, region_names_cortex, region_names_subcortex)
