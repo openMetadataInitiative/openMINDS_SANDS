2250bc088455b03fc739edba430d16376d62d8e8
Kai Kiwitz <kai.kiwitz@gmail.com>
2023-05-08 16:08:43 +0200

    working on parenting structure, adding isNewVersionOf and isALternativeVersionOf to def generate_atlas_versions(entity_path, versions):

--- helper_code/DKT_data_structures.py
+++ helper_code/DKT_data_structures.py
@@ -7,45 +7,53 @@ def data_structures():
     full_documentation =  [{"DKT": "https://doi.org/10.3389/fnins.2012.00171"}]
     main_documentation = "https://doi.org/10.3389/fnins.2012.00171"
     description = "Mindboggle-101 surface and volume brain atlases (anatomical labels from a population of brains)."
-    abbreviation = "DKT"
-    fullName = "Desikan-Killiany-Tourville-Atlas"
-    shortName = "DKT"
+    abbreviation = "DKTA"
+    fullName = "Desikan-Killiany-Tourville Atlas"
+    shortName = "DKT Atlas"
     homepage = "https://osf.io/kgdey/wiki/home/"
-    areas = ["temporalLobe-entorhinalCortex", "temporalLobe-parahippGyrus","temporalLobe-fusiformGyrus",
-             "temporalLobe-supTemporalGyrus", "temporalLobe-midTemporalGyrus", "temporalLobe-infTemporalGyrus",
-             "temporalLobe-transTemporalGyrus", "frontalLobe-supFrontal", "frontalLobe-midFrontalGyrus-rostral",
-             "frontalLobe-midFrontalGyrus-caudal","frontalLobe-infFrontalGyrus-parsOpercularis","frontalLobe-infFrontalGyrus-parsOrbitalis",
-             "frontalLobe-infFrontalGyrus-parsTriangularis","frontalLobe-orbitofrontalGyrus","frontalLobe-lateralDivision","frontalLobe-medialDivision",
-             "frontalLobe-precGyrus","frontalLobe-paracLobule", "parietalLobe-postcGyrus", "parietalLobe-supraMarginalGyrus", "parietalLobe-supParietalLobule", "parietalLobe-infParietalLobule", "parietalLobe-precuneus",
-             "occipitalLobe-lingGyrus", "occipitalLobe-pericalcCortex", "occipitalLobe-cuneus", "occipitalLobe-latOccipitalCortex", "cingulateCortex-rostralAnterior", "cingulateCortex-caudalAnterior", "cingulateCortex-posterior", "cingulateCortex-isthmus"]
-    areas_surf = [s + "_lH" for s in areas] + [s + "_rH" for s in areas]
 
+    # Data Structures for PEs
+    # areas = ["caudalAnteriorCingulate", "caudalMiddleFrontal","cuneus",
+    #          "entorhinal", "fusiform", "inferiorParietal",
+    #          "inferiorTemporal", "isthmusCingulate", "lateralOccipital",
+    #          "lateralOrbitofrontal","lingual","medialOrbitofrontal",
+    #          "middleTemporal","parahippocampal","paracentral","parsOpercularis",
+    #          "parsOrbitalis","parsTriangularis", "pericalcarine", "postcentral", "posteriorCingulate", "precentral", "precuneus",
+    #          "rostralAnteriorCingulate", "rostralMiddleFrontal", "superiorFrontal", "superiorParietal", "superiorTemporal", "supramarginal", "transverseTemporal", "insula"]
 
+    areas_1stParent = ["medialTemporalLobe", "lateralTemporalLobe", "frontalLobe", "parietalLobe", "occipitalLobe", "cingulateCortex"]
+    areas_2ndParent = ["brain"]
+    areas_parent_matched = {"caudalAnteriorCingulate": "cingulateCortex", "caudalMiddleFrontal": "frontalLobe", "cuneus": "occipitalLobe",
+             "entorhinal": "medialTemporalLobe", "fusiform": "medialTemporalLobe", "inferiorParietal": "parietalLobe",
+             "inferiorTemporal": "lateralTemporalLobe", "isthmusCingulate": "cingulateCortex", "lateralOccipital": "occipitalLobe",
+             "lateralOrbitofrontal": "frontalLobe", "lingual": "occipitalLobe", "medialOrbitofrontal": "frontalLobe",
+             "middleTemporal": "lateralTemporalLobe" ,"parahippocampal": "medialTemporalLobe","paracentral": "frontalLobe", "parsOpercularis": "frontalLobe",
+             "parsOrbitalis": "frontalLobe", "parsTriangularis": "frontalLobe", "pericalcarine": "occipitalLobe", "postcentral": "parietalLobe", "posteriorCingulate": "cingulateCortex", "precentral": "frontalLobe", "precuneus": "parietalLobe",
+             "rostralAnteriorCingulate": "cingulateCortex", "rostralMiddleFrontal": "frontalLobe", "superiorFrontal": "frontalLobe", "superiorParietal": "parietalLobe", "superiorTemporal": "lateralTemporalLobe", "supramarginal": "parietalLobe", "transverseTemporal": "lateralTemporalLobe", "insula": None}
+    areas = {outer_key: {outer_value: areas_2ndParent[0]} for outer_key, outer_value in areas_parent_matched.items()}
+
+ # Data Structures for all VERSIONS (BAVs, PEVs)
+
+        # probabilistic Atlas, determinstic atlas
     # versions is a list of all versions stored as dictionaries with reference spaces and areas attached IMPORTANT
-    versions = [{"DKT_dkt31VolLabels": {"reference_space": "MNI_ICBM_152_2009c_nonlin_asym", "areas": areas,
-                                      "accessibility": "freeAccess", "atlasType": "parcellationScheme", "version_identifier": "v1",
+    versions = [{"DKTA_p101-MNI152": {"reference_space": "MNI_ICBM_152_2009c_nonlin_asym", "areas": areas,
+                                      "accessibility": "freeAccess", "atlasType": "parcellationScheme", "version_identifier": "101 participants, MNI",
                                       "version_innovation": "Joint fusion volume atlas version of the DKT atlas",
-                                      "release_date": "2016-07-11", "short_name": "dkt31VolLabels", "homepage": "https://osf.io/kgdey/",
+                                      "release_date": "2016-07-11", "short_name": "DKT Atlas", "homepage": "https://osf.io/kgdey/",
                                       "license":"CC BY-NC-ND 3.0", "digitalIdentifier": "https://doi.org/10.3389/fnins.2012.00171",
-                                      "full_doc_name": "DKT31_vol_labels", "authors": ["kleinArno", "tourvilleJason"]}},
-                {"DKT_dkt31VolProb": {"reference_space" : "MNI_ICBM_152_2009c_nonlin_asym", "areas" : areas,
-                                             "accessibility": "freeAccess", "atlasType": "probabilisticAtlas", "version_identifier": "v2",
-                                             "version_innovation": "Joint fusion label probabilitiy version of the DKT atlas",
-                                             "release_date": "2016-07-11", "short_name": "dkt31VolProb", "homepage": "https://osf.io/kgdey/",
+                                      "full_doc_name": "DKTA_p101-MNI152", "authors": ["kleinArno", "tourvilleJason"], "altVersion": "DKTA_p101-fsaverage"}},
+                {"DKTA_p101-fsaverage": {"reference_space" : "MNI_ICBM_152_2009c_nonlin_asym", "areas" : areas,
+                                             "accessibility": "freeAccess", "atlasType": "parcellationScheme", "version_identifier": "101 participants, fsaverage",
+                                             "version_innovation": "Cortical surface atlas version of the DKT atlas derived from 101 participants",
+                                             "release_date": "2016-07-11", "short_name": "DKT Atlas", "homepage": "https://osf.io/kgdey/",
                                              "license":"CC BY-NC-ND 3.0", "digitalIdentifier": "https://doi.org/10.3389/fnins.2012.00171",
-                                             "full_doc_name": "DKT31_vol_probabilistic", "authors": ["kleinArno", "tourvilleJason"]}},
-                {"DKT_dkt31Surf101": {"reference_space": "fsaverage_5", "areas": areas_surf,
-                                            "accessibility": "freeAccess", "atlasType": "parcellationScheme", "version_identifier": "v3",
-                                            "version_innovation": "Cortical surface atlas version of the DKT atlas from all 101 participants",
-                                            "release_date": "2016-07-11", "short_name": "dkt31Surf101", "homepage": "https://osf.io/kgdey/",
-                                            "license": "CC BY-NC-ND 3.0", "digitalIdentifier": " https://doi.org/10.3389/fnins.2012.00171",
-                                            "full_doc_name": "DKT31_surf_101subjects", "authors": ["kleinArno", "tourvilleJason"]}},
-                {"DKT_dktSurf40": {"reference_space": "fsaverage_5", "areas": areas_surf,
-                                            "accessibility": "freeAccess", "atlasType": "parcellationScheme", "version_identifier": "v4",
+                                             "full_doc_name": "DKTA_p101-fsaverage", "authors": ["kleinArno", "tourvilleJason"], "altVersion": "DKTA_p101-MNI152", "newVersion": "DKTA_p40-fsaverage"}},
+                {"DKTA_p40-fsaverage": {"reference_space": "fsaverage_5", "areas": areas,
+                                            "accessibility": "freeAccess", "atlasType": "parcellationScheme", "version_identifier": "40 participants, fsaverage",
                                             "version_innovation": "Cortical surface atlas version of the DKT atlas from 40 participants",
-                                            "release_date": "2016-07-11", "short_name": "dktSurf40", "homepage": "https://osf.io/kgdey/",
+                                            "release_date": "2016-07-11", "short_name": "DKT Atlas", "homepage": "https://osf.io/kgdey/",
                                             "license": "CC BY-NC-ND 3.0", "digitalIdentifier": " https://doi.org/10.3389/fnins.2012.00171",
-                                            "full_doc_name": "DKT_surf_40sujects", "authors": ["kleinArno", "tourvilleJason"]}}
+                                            "full_doc_name": "DKTA_p40-fsaverage", "authors": ["kleinArno", "tourvilleJason"]}},
                 ]
 
     return (DKT_authors, full_documentation, main_documentation, description, abbreviation, fullName, shortName, homepage,areas, areas_surf, versions)
